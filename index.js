c6415ebb1a227b49ad76cd17078be461805fe7c3
Mohamad Karbejha <78583194+Karbejha@users.noreply.github.com>
2023-10-11 17:05:42 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+customerName = 'bob';
+function upperCaseCustomerName(){
+    if (customerName)
+    {
+        customerName=customerName.toUpperCase();
+    }
+}
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob"
+}
+
+const leastFavoriteCustomer = 'initialValue';
+
+function changeLeastFavoriteCustomer() {
+  try {
+    leastFavoriteCustomer = 'newValue';
+  } catch (error) {
+    throw new Error('Assignment to constant variable.');
+  }
+}
+
+customerName();
+upperCaseCustomerName();
+setBestCustomer();
+overwriteBestCustomer();
+changeLeastFavoriteCustomer();
